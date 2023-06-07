4a55e3f2323d79efd66e20ef2b600d6b46fecf13
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-06-07 16:02:33 +0200

    foreach on array

--- index.php
+++ index.php
@@ -49,11 +49,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hotel list</title>
-    <link  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous" defer></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"
+		defer></script>
 </head>
 <body>
+    <!-- <?php foreach($hotels as $key => $hotelsDescriptions) {
+            foreach($hotelsDescriptions as $key => $hotelsItems){ 
+                echo $hotelsItems;
+            }
+        }
+        ?> -->
     
+
+    <?php foreach($hotels as $key => $hotelsDescriptions) { ?>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Hotel</th>
+                        <th scope="col">description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">Distance from the City Center</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"> <?= $hotelsDescriptions['name'] ?> </th>
+                        <td><?= $hotelsDescriptions['description'] ?></td>  
+                        <td><?= $hotelsDescriptions['parking'] ?></td> 
+                        <td><?= $hotelsDescriptions['vote'] ?></td> 
+                        <td><?= $hotelsDescriptions['distance_to_center'] ?></td>   
+                    </tr>
+                </tbody>
+            </table> <?php
+            
+        } ?>
 </body>
 </html>
